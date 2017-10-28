4652a5128c42226e02d08ce1d5fbdc67e05d2342
lee s. (sl9n17) <sl9n17@soton.ac.uk>
2017-10-28 19:20:30 +0100

    Fixed compilation errors.

--- src/dlx_pipe_if.sv
+++ src/dlx_pipe_if.sv
@@ -12,58 +12,59 @@
 `ifndef DLX_PIPE_IF
 `define DLX_PIPE_IF
 
-`include "dlx_globals.svh"
+`include "../include/dlx_global_pkg.svh"
 
 module dlx_pipe_if (
 		    input logic clk,
 		    input logic rst,
 		    input logic stall,
 		    input logic dc_wait,
-		    input dlx_word id_npc,
+		    input dlx_global_pkg::dlx_word id_npc,
 		    input logic id_cond,		    
-		    input dlx_word ic_data,
-		    output dlx_address ic_addr,
-		    output dlx_word if_id_ir ,
-		    output dlx_word if_id_npc
+		    input  dlx_global_pkg::dlx_word ic_data,
+		    output dlx_global_pkg::dlx_addr ic_addr,
+		    output dlx_global_pkg::dlx_word if_id_ir ,
+		    output dlx_global_pkg::dlx_word if_id_npc
 		    );
 
    //Internal signals
    
-   dlx_word pc; //program counter
-   dlx_word npc;
+   dlx_global_pkg::dlx_word pc; //program counter
+   dlx_global_pkg::dlx_word npc;
          
    always_comb begin: if_comb
       ic_addr = pc;
    end
 
    always_ff @ (posedge clk , posedge rst ) begin : if_seq      
-      if(rst == 1'b1) {
+      if(rst == 1'b1) begin
 	 pc <= '0;
 	 if_id_ir <= '0;	 
-      }
-      else {
+      end
+      else begin
 	 //Instruction cache miss, if and id stalled for branch instrunctions
 	 //Rest of pipe can continue and resolver data dependencies
-	 if(dc_wait == 1'b0 && stall == 1'b0){
+	 if(dc_wait == 1'b0 && stall == 1'b0)begin
 	    if_id_ir <= ic_data;
-	    if(id_cond == 1'b0){
+	    if(id_cond == 1'b0)begin
 	       npc = pc + 4; 
-            }
-	    else if (id_cond = 1'b1){
+            end
+	    else if (id_cond == 1'b1)begin
 		    npc = id_npc;
-            }
-	    else{
+            end
+	    else begin
 	       npc <= 'x; // default x propagation
-            }
+            end
             if_id_npc <= npc;
             pc <= npc;  
-        }
-      }
+        end
+      end
    end : if_seq
    
 endmodule
   
-   
+`endif
+
 		    
 
 		   
